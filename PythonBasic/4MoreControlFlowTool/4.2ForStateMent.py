1572b3501ed11b4584087e0d0c815168239514a5
DuongLieuMoc <kerfspecter@gmail.com>
2024-08-21 21:27:38 +0700

    Few Basic

---
+++ PythonBasic/4MoreControlFlowTool/4.2ForStateMent.py
@@ -0,0 +1,5 @@
+words = ['cat', 'window', 'defenestrate'] #Create List
+
+for w in words: #assign every element in list to w 
+    print(w, len(w)) #print w, lenght of the words using len() function
+
