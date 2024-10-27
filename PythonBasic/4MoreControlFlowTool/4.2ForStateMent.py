36684a73c8290adb5db51d71c0763067237ad044
DuongLieuMoc <kerfspecter@gmail.com>
2024-10-27 11:28:20 +0700

    ChaoEmGai

--- PythonBasic/4MoreControlFlowTool/4.2ForStateMent.py
+++
@@ -1,5 +0,0 @@
-words = ['cat', 'window', 'defenestrate'] #Create List
-
-for w in words: #assign every element in list to w 
-    print(w, len(w)) #print w, lenght of the words using len() function
-
--- PythonBasic/4MoreControlFlowTool/4.6.1PS.py
+++
@@ -1,8 +0,0 @@
-def http_error(status):
-    match status:
-        case 401 | 403 | 404:
-            return "Not allowed"
-        
-http = http_error
-
-print(http(403))
